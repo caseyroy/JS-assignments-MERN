a4017b43a891a5c6c6105ba41c162080bf738fd9
caseyroy <65331620+caseyroy@users.noreply.github.com>
2020-07-07 16:32:44 -0700

    ninja update

--- MERN/JS assignments/ninja.js
+++ MERN/JS assignments/ninja.js
@@ -1,3 +1,18 @@
-const ninja1 = new Ninja("Hyabusa");
+class Ninja {
+    constructor(name, health = 25, speed = 3, strength = 3){
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+    sayName(){
+        console.log(`Name:${this.name}`)
+    }
+    showStats(){
+        console.log(`Health:${this.health}\nSpeed:${this.speed}\nStrength:${this.strength}`)
+    }
+}
+
+const ninja1 = new Ninja("Diamond Dave");
 ninja1.sayName();
 ninja1.showStats();
