36ef0ab212f8ac32ee1fbc8b03a93e394540b629
caseyroy <65331620+caseyroy@users.noreply.github.com>
2020-07-08 18:43:28 -0700

    capitalization kills

---
+++ MERN/React assignments/propitup/src/components/newComp1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewComp1 extends Component {
+    render() {
+        return (
+            <div>
+                We are in the new capitalized NewComp1
+            </div>
+        );
+    }
+}
+
+export default NewComp1
