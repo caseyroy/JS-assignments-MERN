604c09d1f5c3d042d4fa83d203db9f56ce5d4ae9
caseyroy <65331620+caseyroy@users.noreply.github.com>
2020-07-09 19:03:56 -0700

    changed it to the function components, finished putting it together

--- MERN/React assignments/propitup/src/components/PersonCard.js
+++ MERN/React assignments/propitup/src/components/PersonCard.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
-class PersonCard extends Component {
-    render() {
-        return (
-            <div>
-                <ul id="list">
-                    <li><h2>Name:{this.props.lastname}, {this.props.firstname}</h2></li>
-                    <li>Age: {this.props.age}</li>
-                    <li>Hair Color: {this.props.haircolor}</li>
-                </ul>
-
-            </div>
-        );
+const PersonCard = props => {
+    const [age, setAge] = useState(props.age);
+    const onclickhandler = () => {
+        setAge(age + 1)
     }
+
+
+    return (
+        <div>
+            <ul id="list">
+                <li><h2>Name:{props.lastname}, {props.firstname}</h2></li>
+                <li>Age: {age}</li>
+                <li>Hair Color: {props.haircolor}</li>
+                <li><button onClick={onclickhandler}>Birthday Button for {props.firstname} {props.lastname}</button></li>
+            </ul>
+        </div>
+
+    );
 }
 
 export default PersonCard
